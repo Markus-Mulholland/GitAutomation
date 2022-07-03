822109c0e869e1f34c2a5572313c968396f18463
LAPTOP-ITGLCJKU\Markus <markus@sasoft.co.za>
2022-07-03 21:54:12 +0200

    Finalized script that handles setting up a local branch on the remote

--- g-set-u.sh
+++ g-set-u.sh
@@ -1,8 +1,9 @@
 #!C:/Program\ Files/Git/bin/sh.exe
 
 status=$(git status)
-echo $branch
 
 branch=$(echo $status | cut -d' ' -f 3)
 
-echo "git push --set-upstream origin ${branch}"
+command="git push --set-upstream origin ${branch}"
+
+eval $command
